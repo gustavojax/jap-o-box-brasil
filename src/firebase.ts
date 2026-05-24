d4d8f41c6b614c631921dd49ffce8d35de0fda6a
gustavojax <54778384+gustavojax@users.noreply.github.com>
2026-05-24 04:46:07 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,7 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// =========================================
-// CONFIG FIREBASE (CORRIGIDO)
-// =========================================
 const firebaseConfig = {
   apiKey: "AIzaSyCoPztqmG0b7lAIaWRg-iIVvQgsJLkV5_I",
   authDomain: "jap-box-core-prod.firebaseapp.com",
@@ -14,13 +11,7 @@ const firebaseConfig = {
   appId: "1:842214047732:web:2ab310e943ef47f1c303a1",
 };
 
-// =========================================
-// EVITA REINICIALIZAÇÃO (IMPORTANTE EM DEV/VERCEL)
-// =========================================
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// =========================================
-// EXPORTS
-// =========================================
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
