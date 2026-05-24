295c73d49a6d3996a512efcced2c4377b7cfd8c2
gustavojax <54778384+gustavojax@users.noreply.github.com>
2026-05-24 03:12:11 -0300

    Update tracking.ts

--- src/services/tracking.ts
+++ src/services/tracking.ts
@@ -2,10 +2,16 @@ import { db } from "../firebase";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import type { TrackingData } from "../types";
 
+// ============================
+// SALVAR TRACKING (ADMIN)
+// ============================
 export async function saveTracking(data: TrackingData) {
   await setDoc(doc(db, "tracking", data.code), data);
 }
 
+// ============================
+// BUSCAR TRACKING (USUÁRIO)
+// ============================
 export async function getTracking(code: string) {
   const ref = doc(db, "tracking", code);
   const snap = await getDoc(ref);
