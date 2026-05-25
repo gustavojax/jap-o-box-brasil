c9888d9683ba03ee9a36d533846fe82d30bc98a7
gustavojax <54778384+gustavojax@users.noreply.github.com>
2026-05-25 07:43:34 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,30 @@
+export type MainDepartment =
+  | "Casa e Cozinha"
+  | "Alimentos e Limpeza"
+  | "Tecnologia, Ferramentas e Automotivo"
+  | "Beleza, Higiene e Saúde"
+  | "Moda e Acessórios"
+  | "Estilo de Vida, Cultura e Exclusivos";
+
 export interface Product {
   id: string;
   name: string;
   jpName: string;
   description: string;
-
-  category: string;
-  subcategory?: string;
-
   priceBRL: number;
   serviceFeeBRL: number;
   shippingEstBRL: number;
-  estimatedTaxBRL: number;
-
+  estimatedTaxBRL?: number;
+  image: string;
   rating: number;
+  reviewsCount?: number;
+  department: MainDepartment;
+  category: string; // Corresponde exatamente às subcategorias desejadas
+  stock: number;
 }
 
 export interface CartItem {
   product: Product;
   quantity: number;
-  selectedUpsells: any[];
+  selectedUpsells?: string[];
 }
