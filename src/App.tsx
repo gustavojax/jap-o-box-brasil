59be2f9d0aeaa77d77d9723107ef9741d56af4ea
gustavojax <54778384+gustavojax@users.noreply.github.com>
2026-05-25 20:58:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-Fimport React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
